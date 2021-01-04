c23d398716bb4a463fe410fb5a4194561e72443b
debajyoti <debajyoti94@gmail.com>
2021-01-04 21:54:57 +0530

    Added code for deriving parameters using: 	-Normal equation 	-Gradient descent

--- src/models.py
+++ src/models.py
@@ -1,4 +1,66 @@
 ''' This file will contain the code for implementing linear regression model using
-- OLS Ordinary least squares method
 - Gradient descent technique'''
+
+
+from config import learning_rate,num_of_epochs, model_name_OLS, model_name_GD
+import numpy as np
+
+class LinearRegressionFromScratch:
+
+    def __init__(self):
+        self.num_of_epochs = num_of_epochs
+        self.model_name_OLS = model_name_OLS
+        self.model_name_GD = model_name_GD
+
+    def lr_normal_equation(self, wine_input_features, wine_quality_ouput):
+        '''
+        theta = (X^T*X)^-1*X*y, this is the normal equation
+        :param wine_input_features: the input features
+        :param wine_quality_ouput:  the wine quality-- output
+        :return: theta-- set of parameters
+        '''
+        # converting the dataframe to numpy array
+        X = wine_input_features.to_numpy()
+        y = wine_quality_ouput.to_numpy()
+
+        #applying the normal equation below @ is used for matrix multiplication
+        theta = np.linalg.inv(X.T@X)@X@y
+
+        return theta
+
+
+    def cost_function(self, theta, wine_input_features, wine_quality_output):
+        '''
+        Calculating MSE
+        :param theta: model parameters
+        :param wine_input_features: input
+        :param wine_quality_output: output/predictions
+        :return:
+        '''
+        predictions = wine_input_features @ theta
+        squared_errors = np.square(predictions - wine_quality_output)
+
+        return np.sum(squared_errors)/(2*len(wine_quality_output))
+
+    def lr_gradient_descent(self, wine_input_features, wine_quality_output, lr):
+        '''
+        function for gradient descent
+        :param wine_input_features:
+        :param wine_quality_output:
+        :param lr: learning rate
+        :return:
+        '''
+        X = wine_input_features.to_numpy()
+        y = wine_quality_output.to_numpy()
+
+        cost_history = np.zeros(self.num_of_epochs)  # create a vector to store the cost history
+        m = y.size  # number of training examples
+
+        for i in range(self.num_of_epochs):
+            predictions = np.dot(X, theta)
+            theta = theta - lr * (1.0 / m) * np.dot(X.T, predictions - y)
+            cost_history[i] = self.cost(theta, X, y)  # compute and record the cost
+
+        return theta, cost_history
+
 
