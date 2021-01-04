3a938887f7e849dfb710b108ff42cb3010a8bbc1
debajyoti <debajyoti94@gmail.com>
2021-01-04 23:53:32 +0530

    Made some changes here. Will probably revert back later.

--- src/config.py
+++ src/config.py
@@ -4,8 +4,7 @@ This file will contain the variables which are useful for training the model.'''
 model_name_OLS = '../models/LR_OLS.pickle'
 model_name_GD = '../models/LR_GD.pickle'
 
-learning_rate = 0.05    # NEEDED WHILE TRAINING THE MODEL
-num_of_epochs = 100
+num_of_epochs = 100     #USED in gradient descent
 
 train_set = '../input/wine_train_set.pickle'
 test_set = '../input/wine_test_set.pickle'
